dd4c2923f7c686cf9475852c6d3a2871970b590b
mehdi-larek <larek.med@gmail.com>
2025-07-11 11:36:19 +0200

    init

---
+++ src/application/use-cases/product/create-product.usecase.ts
@@ -0,0 +1,7 @@
+export class CreateProductUseCase {
+    //constructor(private readonly productRepository: ProductRepository) {}
+
+    // async execute(data: CreateProductInputDto): Promise<ProductOutputDto> {
+
+    // }
+}
