bc231ab19bb67d77e1e04fb4a14ad561e19a8392
mehdi-larek <larek.med@gmail.com>
2025-07-11 14:07:23 +0200

    ajout d'un ProductOrmAdapter pour l'import dans app.module

--- src/domain/repositories/product.repository.ts
+++ src/domain/repositories/product.repository.ts
@@ -7,7 +7,8 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export abstract class ProductRepository {
 
-    abstract create(data: CreateProductInputDto): Promise<Product>
+    //abstract create(data: CreateProductInputDto): Promise<Product>
+    
     // abstract findAll(): Promise<Product[]>
     // abstract findById(id: number): Promise<Product|null>
     // abstract findAllByIds(ids: number[]): Promise<Product[]>
